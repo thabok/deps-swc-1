c915e0dda003bd97136bf5e6ef00135151f6bf6d
Thabo Krick <thabokrick@gmail.com>
2023-12-14 16:10:17 +0100

    Added conanfile.py, models and extended gitignore

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# deps-swc-1
-Dependency Management Demo: Software Component 1 (uses a shared module)
+# SWC 1
+Dependency Management Demo: Software Component 1 (uses a shared module called **shared_module_a**)
+
+## Preparation
+You can resolve the SWC's dependencies by calling 
+```sh
+conan install . -r conanrepo
+```
+
+If you haven't configured conan yet, point it to the right repository by running "conan remote add". Here's an example for a conan repo in artifactory:
+```sh
+conan remote add conanrepo http://$host:$port/artifactory/api/conan/$reponame
+```
---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.files import copy
+
+
+class SWC1(ConanFile):
+    name = "swc_1"
+    version = "1.0.0"
+    license = "MIT"
+    url = "https://github.com/thabok/deps-swc-1"
+    description = "Conan package for SWC1"
+    requires = "shared_module_a/1.0.1@thabok/testing"
+
+
+    def generate(self):
+        # disable generation of scripts in the build folder
+        VirtualBuildEnv(self)
+        VirtualRunEnv(self)
+
+        # copy model files from dependencies root folder into
+        # folders with the dep-name inside the build_folder
+        for dep in self.dependencies.values():
+            copy(self, "*.slx", dep.package_folder, join(self.build_folder, 'shared', dep.ref.name))
+
