ea9c8a0230d9f9246da63c64cfd39749ea25e79f
Thabo Krick <thabokrick@gmail.com>
2023-12-15 16:10:35 +0100

    removed obsolete entries from project, made startup script more robust

--- startup.m
+++ startup.m
@@ -1,1 +1,14 @@
-!conan install . &> conan.log
+% check if conan is available
+[status, ~] = system('which conan');
+if status ~= 0 && ~contains(getenv('PATH'), '/opt/homebrew/bin')
+    % add homebrew bin folder (needed to work on mac os)
+    setenv('PATH', [getenv('PATH') ':/opt/homebrew/bin']);
+    disp('added homebrew stuff to path');
+end
+
+% run conan install and dump output to log file
+[status, ~] = system('conan install . &> conan.log');
+if status ~= 0
+    % warn in case of problems
+    disp('Could not resolve dependencies with Conan.');
+end
